860f14479fd0dccb25f73490508c435ab2f0765b
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2025-04-05 23:34:35 +0530

    Promise understanding

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-purchase(true, second)
+// Look at both the commented lines,
+// both are correct try to understand WHY? 
+// Remember these functions return promise object.
+purchase(true)
+    .then((oId) => { return proceedToPayment(oId) })
+    // .then((oId) => proceedToPayment(oId))
+    // .then(proceedToPayment)
+    .then(second)
     .catch(err => {
         console.log(err);
     });
 
+
+function proceedToPayment(orderId) {
+    return new Promise((res, rej) => {
+        if (orderId)
+            res(orderId * 10);
+    });
+}
+
 function purchase(valid, callback) {
     const promise = new Promise((resolve, reject) => {
-        if (valid) resolve('Purchased!');
+        if (valid) resolve(11);
         else reject('Invalid purchase!!!');
     });
-
-    // If a callback is provided, invoke it when the promise resolves
-    if (callback) {
-        promise.then(callback);
-    }
 
     return promise;
 }
